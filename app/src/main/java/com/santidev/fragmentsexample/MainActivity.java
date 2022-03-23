be9b835294c2eacafec6739db598c9fd2b413e3f
santiDev <androidsan94@gmail.com>
2022-03-23 16:16:32 -0600

    finish

--- app/src/main/java/com/santidev/fragmentsexample/MainActivity.java
+++ app/src/main/java/com/santidev/fragmentsexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.santidev.fragmentsexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
@@ -9,19 +10,32 @@ import com.santidev.fragmentsexample.Fragments.DetailsFragment;
 
 public class MainActivity extends FragmentActivity implements DataFragment.DataListener {
 
+    private boolean isMultipanel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setMultipanel();
     }
 
     @Override
     public void sendData(String text) {
         // Llamar al metodo renderizarTexto del DetailsFragment,
         // pasandole el texto que recibamos por parametro en este mismo metodo
+        if(this.isMultipanel) {
+            DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
+            detailsFragment.renderText(text);
+        }else {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            intent.putExtra("text", text);
+            startActivity(intent);
 
-        DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager().findFragmentById(R.id.detailsFragment);
-        detailsFragment.renderText(text);
+        }
+    }
 
+    private void setMultipanel(){
+        this.isMultipanel = (getSupportFragmentManager().findFragmentById(R.id.detailsFragment) != null);
     }
 }
